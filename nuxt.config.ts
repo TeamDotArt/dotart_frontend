c2d0ae9b785b97e62d72dc5c47398ef92c750050
RimlTempest <riml.slime@gmail.com>
2021-07-28 00:20:02 +0900

    update: lib

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,4 @@
-import { Configuration } from '@nuxt/types';
+import { NuxtConfig } from '@nuxt/types';
 import colors from 'vuetify/es5/util/colors';
 import { Auth } from 'nuxtjs__auth';
 
@@ -9,8 +9,9 @@ declare module 'vue/types/vue' {
 }
 
 const manifestIcon = 'static/icon.png';
-const nuxtConfig: Configuration = {
-    mode: 'spa',
+const nuxtConfig: NuxtConfig = {
+    target: 'static',
+    ssr: false,
     /*
      ** Headers of the page
      */
