d58e2f7fa2950259ac939e870f3e3f655f887836
RimlTempest <riml.slime@gmail.com>
2021-10-07 14:06:06 +0900

    add: コンポーネントカタログの追加

---
+++ components/Atomics/RedoButton.stories.ts
@@ -0,0 +1,10 @@
+import RedoButton from '@/components/Atomics/RedoButton.vue';
+
+export default {
+    title: 'Atomics/RedoButton',
+};
+
+export const Default = () => ({
+    components: { RedoButton },
+    template: '<RedoButton />',
+});
---
+++ components/Home/Info.stories.ts
@@ -0,0 +1,10 @@
+import Info from '@/components/Home/Info.vue';
+
+export default {
+    title: 'Home/Info',
+};
+
+export const Default = () => ({
+    components: { Info },
+    template: '<Info />',
+});
