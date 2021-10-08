63931d0edb92106fbb03f23f8a53060a824c0a32
RimlTempest <riml.slime@gmail.com>
2021-10-08 15:14:19 +0900

    Add: afterdrawのcomposablesを追加

---
+++ composables/useAfterDraw.ts
@@ -0,0 +1,27 @@
+// ホーム画面へ遷移するロジック
+import { Stack } from '@/types/Canvas/StackType';
+
+type StackType = {
+    undoRedoDataIndex: number;
+    undoRedoDataStack: Stack[];
+    stackMaxSize: number;
+    canvasIndexData: number[];
+};
+
+const useAfterDraw = (stackData: StackType): void => {
+    // やり直しをした後だった場合、現在の表示内容以降のデータは削除
+    if (stackData.undoRedoDataIndex < stackData.undoRedoDataStack.length - 1) {
+        stackData.undoRedoDataStack.splice(stackData.undoRedoDataIndex + 1);
+    }
+    // 巻き戻し最大回数より多かったら先頭を削除、そうでなければ追加
+    if (stackData.undoRedoDataIndex >= stackData.stackMaxSize) {
+        stackData.undoRedoDataStack.shift();
+    } else {
+        ++stackData.undoRedoDataIndex;
+    }
+    // データのプッシュ
+    stackData.undoRedoDataStack.push({
+        indexData: stackData.canvasIndexData.slice(),
+    });
+};
+export default useAfterDraw;
