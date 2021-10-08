c6d852af876c16a50774650c11c5319a8bbff1fa
RimlTempest <riml.slime@gmail.com>
2021-10-08 15:15:10 +0900

    Add: 再描画のcomposablesを追加

---
+++ composables/useReDraw.ts
@@ -0,0 +1,29 @@
+// ホーム画面へ遷移するロジック
+
+type CanvasType = {
+    canvasCtx: CanvasRenderingContext2D | null;
+    canvasRange: number;
+    canvasIndexData: number[];
+    canvasMagnification: number;
+    colorPallet: string[];
+};
+
+const useReDraw = (canvasData: CanvasType, indexData: number[]): void => {
+    for (let x = 0; x < canvasData.canvasRange; x++) {
+        for (let y = 0; y < canvasData.canvasRange; y++) {
+            canvasData.canvasCtx!.fillStyle =
+                canvasData.colorPallet[
+                    indexData[y * canvasData.canvasRange + x]
+                ];
+            canvasData.canvasCtx!.fillRect(
+                x * canvasData.canvasMagnification,
+                y * canvasData.canvasMagnification,
+                canvasData.canvasMagnification,
+                canvasData.canvasMagnification
+            );
+            canvasData.canvasIndexData[y * canvasData.canvasRange + x] =
+                indexData[y * canvasData.canvasRange + x];
+        }
+    }
+};
+export default useReDraw;
