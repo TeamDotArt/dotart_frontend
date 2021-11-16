4a1a5fcb07054cea724199bdce2429f27c8e80e5
zinNORI <zin9349@outlook.com>
2021-11-16 16:10:38 +0900

    add:バケツとペンのボタン追加

---
+++ components/Atomics/BucketButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <button-base :click="clickEvent">
+        <icon-Base width="32" height="32">
+            <bucketIcon />
+        </icon-Base>
+    </button-base>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+import IconBase from '@/components/Atomics/icons/IconBase.vue';
+import ButtonBase from '@/components/Atomics/ButtonBase.vue';
+import BucketIcon from '@/components/Atomics/icons/BucketIcon.vue';
+
+export default defineComponent({
+    name: 'PenButton',
+    components: {
+        IconBase,
+        BucketIcon,
+        ButtonBase,
+    },
+    props: {
+        clickEvent: {
+            type: Function,
+            required: true,
+        },
+    },
+});
+</script>
+<style lang="scss" scoped>
+/* */
+</style>
---
+++ components/Atomics/PenButton.vue
@@ -0,0 +1,32 @@
+<template>
+    <button-base :click="clickEvent">
+        <icon-Base width="32" height="32">
+            <penIcon />
+        </icon-Base>
+    </button-base>
+</template>
+
+<script lang="ts">
+import { defineComponent } from '@nuxtjs/composition-api';
+import IconBase from '@/components/Atomics/icons/IconBase.vue';
+import ButtonBase from '@/components/Atomics/ButtonBase.vue';
+import PenIcon from '@/components/Atomics/icons/PenIcon.vue';
+
+export default defineComponent({
+    name: 'PenButton',
+    components: {
+        IconBase,
+        PenIcon,
+        ButtonBase,
+    },
+    props: {
+        clickEvent: {
+            type: Function,
+            required: true,
+        },
+    },
+});
+</script>
+<style lang="scss" scoped>
+/* */
+</style>
