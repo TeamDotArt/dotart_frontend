626e733f6a9904466d3407954eb705d5aa83b024
RimlTempest <riml.slime@gmail.com>
2022-01-31 16:56:31 +0900

    add: SNS連携、通信ローダを実装

--- pages/creator/save.vue
+++ pages/creator/save.vue
@@ -57,10 +57,48 @@
                 </v-row>
             </v-container>
         </v-flex>
+        <!-- アップロード待機 -->
+        <div v-if="imgurState.isLoading" class="loaderContainer">
+            <div class="loaderContainer__loader">読込中....</div>
+        </div>
+        <!-- シェアメニュー -->
+        <aside class="snsContainer">
+            <div class="snsContainer__sns_button_group__container">
+                <span
+                    className="snsContainer__sns_button_group__container__title"
+                    >共有</span
+                >
+                <span class="snsContainer__sns_button_group__container__icon">
+                    <a
+                        :href="getTwitterShare"
+                        target="_blank"
+                        className="snsContainer__sns_button_group__container__button"
+                    >
+                        <v-icon>mdi-twitter</v-icon>
+                    </a>
+                </span>
+
+                <span class="snsContainer__sns_button_group__container__icon">
+                    <a
+                        :href="getLINEShare"
+                        target="_blank"
+                        className="snsContainer__sns_button_group__container__button"
+                    >
+                        <img
+                            alt="line"
+                            width="25"
+                            height="25"
+                            src="https://developers.line.biz/media/line-social-plugins/round-grey.png"
+                        />
+                    </a>
+                </span>
+            </div>
+        </aside>
     </v-layout>
 </template>
 
 <script lang="ts">
+import axios from 'axios';
 import {
     defineComponent,
     reactive,
@@ -104,6 +142,18 @@ export default defineComponent({
         });
         const getLayerdCanvasIndexData = computed((): layerdCanvasData[] => {
             return session.canvasData.canvasesIndexData;
+        });
+        const getTwitterShare = computed((): string => {
+            const textAndHashTag = encodeURIComponent(
+                `ドット絵を描いたよ！\n${imgurState.imgurLink} #dotArt\n`
+            );
+            return `https://twitter.com/intent/tweet?url=https://dotart.riml.work&text=${textAndHashTag}`;
+        });
+        const getLINEShare = computed((): string => {
+            const content = encodeURIComponent(
+                `ドット絵を描いたよ！\n${imgurState.imgurLink}\nDotArtはこちらから！\n`
+            );
+            return `https://social-plugins.line.me/lineit/share?url=https://dotart.riml.work&text=${content}`;
         });
         const canvasState = reactive<{
             canvasName: ComputedRef<string>;
@@ -154,6 +204,17 @@ export default defineComponent({
                     magnification: 1,
                 },
             ],
+        });
+        const imgurState = reactive<{
+            imgurId: string;
+            imgurLink: string;
+            imgurDeleteHash: string;
+            isLoading: boolean;
+        }>({
+            imgurId: '',
+            imgurLink: '',
+            imgurDeleteHash: '',
+            isLoading: true,
         });
 
         const layerdDraw = (
@@ -233,7 +294,39 @@ export default defineComponent({
                 }
                 return 0;
             });
+
+            setImgur();
         });
+
+        const setImgur = async (): Promise<void> => {
+            canvasState.saveCanvas!.width =
+                canvasState.canvasRange *
+                setCanvasState.selectedSize.magnification;
+            canvasState.saveCanvas!.height =
+                canvasState.canvasRange *
+                setCanvasState.selectedSize.magnification;
+            // 画像を拡大するのではなく倍率を変えて再描画してるので画質の劣化はないと思う
+            layerdDraw(
+                canvasState.canvasRange,
+                setCanvasState.selectedSize.magnification,
+                canvasState.saveCanvasCtx
+            );
+            const img = canvasState.saveCanvas!.toDataURL('image/png')!;
+            try {
+                const res = await axios.post(
+                    'https://dotart-backend.herokuapp.com/api/v1/image-uploader',
+                    {
+                        image: img,
+                    }
+                );
+                imgurState.imgurId = res.data.data.id;
+                imgurState.imgurLink = res.data.data.link;
+                imgurState.imgurDeleteHash = res.data.data.deletehash;
+            } catch (e) {
+                alert('画像のアップロードに失敗しました');
+            }
+            imgurState.isLoading = false;
+        };
 
         // 選んだ倍率からドット絵を描画し、画像として保存する
         const saveImage = (): void => {
@@ -274,8 +367,11 @@ export default defineComponent({
             getMagnification,
             getColorPallet,
             getCanvasIndexData,
+            getTwitterShare,
+            getLINEShare,
             canvasState,
             setCanvasState,
+            imgurState,
             saveImage,
         };
     },
@@ -301,5 +397,180 @@ export default defineComponent({
 
 .SaveCanvas {
     display: none;
+}
+
+.snsContainer {
+    position: fixed;
+    background-color: #ce93d8;
+    border-radius: 5px;
+    right: 0;
+    bottom: 0%;
+    width: 3rem;
+    transform: translateY(-50%);
+    z-index: 50;
+    &__sns_button_group__container {
+        align-items: center;
+        border-radius: 5px;
+        display: flex;
+        flex-direction: column;
+        flex-wrap: wrap;
+        justify-content: center;
+        padding: 8px;
+        width: 2em;
+        color: white;
+        &__title {
+            font-size: 12px;
+            margin-bottom: 10px;
+            margin-top: 5px;
+            -ms-writing-mode: tb-rl;
+            writing-mode: vertical-rl;
+        }
+
+        &__icon {
+            color: white;
+            margin-bottom: 5px;
+            margin-top: 5px;
+            padding: 5px;
+        }
+        &__button {
+            background-color: transparent;
+            //width: 30px;
+            //height: 30px;
+            cursor: pointer;
+            line-height: 1;
+
+            &__primary {
+                color: white;
+            }
+
+            &__secondary {
+                color: white;
+            }
+
+            &__small {
+                font-size: 12px;
+            }
+
+            &__medium {
+                font-size: 14px;
+            }
+
+            &__large {
+                font-size: 16px;
+            }
+        }
+    }
+}
+
+.loaderContainer {
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    top: 0;
+    left: 0;
+    right: 0;
+    bottom: 0;
+    background: #000;
+    opacity: 0.5;
+
+    &__loader {
+        color: #ce93d8;
+        font-size: 90px;
+        text-indent: -9999em;
+        overflow: hidden;
+        width: 1em;
+        height: 1em;
+        border-radius: 50%;
+        margin: 72px auto;
+        position: relative;
+        -webkit-transform: translateZ(0);
+        -ms-transform: translateZ(0);
+        transform: translateZ(0);
+        -webkit-animation: load6 1.7s infinite ease, round 1.7s infinite ease;
+        animation: load6 1.7s infinite ease, round 1.7s infinite ease;
+    }
+    @-webkit-keyframes load6 {
+        0% {
+            box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em,
+                0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+        }
+        5%,
+        95% {
+            box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em,
+                0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+        }
+        10%,
+        59% {
+            box-shadow: 0 -0.83em 0 -0.4em, -0.087em -0.825em 0 -0.42em,
+                -0.173em -0.812em 0 -0.44em, -0.256em -0.789em 0 -0.46em,
+                -0.297em -0.775em 0 -0.477em;
+        }
+        20% {
+            box-shadow: 0 -0.83em 0 -0.4em, -0.338em -0.758em 0 -0.42em,
+                -0.555em -0.617em 0 -0.44em, -0.671em -0.488em 0 -0.46em,
+                -0.749em -0.34em 0 -0.477em;
+        }
+        38% {
+            box-shadow: 0 -0.83em 0 -0.4em, -0.377em -0.74em 0 -0.42em,
+                -0.645em -0.522em 0 -0.44em, -0.775em -0.297em 0 -0.46em,
+                -0.82em -0.09em 0 -0.477em;
+        }
+        100% {
+            box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em,
+                0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+        }
+    }
+    @keyframes load6 {
+        0% {
+            box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em,
+                0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+        }
+        5%,
+        95% {
+            box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em,
+                0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+        }
+        10%,
+        59% {
+            box-shadow: 0 -0.83em 0 -0.4em, -0.087em -0.825em 0 -0.42em,
+                -0.173em -0.812em 0 -0.44em, -0.256em -0.789em 0 -0.46em,
+                -0.297em -0.775em 0 -0.477em;
+        }
+        20% {
+            box-shadow: 0 -0.83em 0 -0.4em, -0.338em -0.758em 0 -0.42em,
+                -0.555em -0.617em 0 -0.44em, -0.671em -0.488em 0 -0.46em,
+                -0.749em -0.34em 0 -0.477em;
+        }
+        38% {
+            box-shadow: 0 -0.83em 0 -0.4em, -0.377em -0.74em 0 -0.42em,
+                -0.645em -0.522em 0 -0.44em, -0.775em -0.297em 0 -0.46em,
+                -0.82em -0.09em 0 -0.477em;
+        }
+        100% {
+            box-shadow: 0 -0.83em 0 -0.4em, 0 -0.83em 0 -0.42em,
+                0 -0.83em 0 -0.44em, 0 -0.83em 0 -0.46em, 0 -0.83em 0 -0.477em;
+        }
+    }
+    @-webkit-keyframes round {
+        0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+        100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+        }
+    }
+    @keyframes round {
+        0% {
+            -webkit-transform: rotate(0deg);
+            transform: rotate(0deg);
+        }
+        100% {
+            -webkit-transform: rotate(360deg);
+            transform: rotate(360deg);
+        }
+    }
 }
 </style>
